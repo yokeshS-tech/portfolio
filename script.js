9df267d17403b46645263121689c6ec712b11b9b
yokeshS-tech <yokeshtechy@gmail.com>
2024-12-25 15:24:08 +0530

    updating info

--- script.js
+++ script.js
@@ -59,13 +59,13 @@ ScrollReveal({
  ScrollReveal().reveal(' .home-content h1,.profile,.about-img img', { origin: 'left' });
  ScrollReveal().reveal('.about-content p,.about-content h3,.home-content p,.about-content .btn', { origin: 'right' });
 
- /*==== download cv ====*/
- document.getElementById("downloadBtn").addEventListener("click",
-    function() {
-        const filePath = "portfolio/yokesh_Web Developer resume.pdf";
-        const link = document.createElement("a");
-        link.href = filePath;
-        link.download = "yokesh_Web Developer resume.pdf";
-        link.click();
-    }
- );
+//  /*==== download cv ====*/
+//  document.getElementById("downloadBtn").addEventListener("click",
+//     function() {
+//         const filePath = "portfolio/yokesh_Web Developer resume.pdf";
+//         const link = document.createElement("a");
+//         link.href = filePath;
+//         link.download = "yokesh_Web Developer resume.pdf";
+//         link.click();
+//     }
+//  );
